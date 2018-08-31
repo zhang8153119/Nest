1ff0786ed3952028af8fb613495eacb90ac7d2a9
master <zhang8153119@sina.com>
2018-08-31 14:07:07 +0800

    ？？

---
+++ FZYK.Nest/FrmNest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System .Collections .Generic;
+using System .ComponentModel;
+using System .Data;
+using System .Drawing;
+using System .Linq;
+using System .Text;
+using System .Windows .Forms;
+using FZYK .Core;
+
+namespace FZYK .Nest
+{
+      public partial class FrmNest : BaseSingle
+      {
+            public FrmNest()
+            {
+                  InitializeComponent();
+            }
+      }
+}
